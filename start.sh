138b3e7d0a0933083bab0b7b590090b507150403
Adam Borek <adam@attio.com>
2025-12-21 12:34:14 +0100

    adds start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,6 @@
-docker-compose up -d --no-build
+REMOTE_USER="adam"
+REMOTE_HOST="192.168.1.125"
+REMOTE_PATH="Apps/price-checker/"
+IMAGE_NAME="price-checker"
+
+ssh -l $REMOTE_USER $REMOTE_HOST "source ~/.bashrc && cd $REMOTE_PATH && docker-compose down && docker-compose up -d --no-build"
